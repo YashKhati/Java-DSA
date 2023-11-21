12889f768e791ac032fde5f273c72377e3ed38e4
yash <khatiyash010703@gmail.com>
2023-11-21 09:56:33 +0530

    Pushed From intellj IDEA

---
+++ src/ArraysQuestions/MinimumAmountOfTimeToCollectGarbage.java
@@ -0,0 +1,45 @@
+package ArraysQuestions;
+
+public class MinimumAmountOfTimeToCollectGarbage {
+    public static void main(String[] args) {
+
+        int[] travel = {2,4,3};
+        String[] garbage= {"G","P","GP","GG"};
+
+        int ans = garbageCollection(garbage,travel);
+        System.out.println(ans);
+
+    }
+    public static int garbageCollection(String[] garbage, int[] travel) {
+        int n = garbage.length;
+        int ans = 0;
+        for (int i = 0; i < n - 1; i++) {
+            ans += 3 * travel[i];
+        }
+        for (String s : garbage) {
+            ans += s.length();
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("G")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
+            }
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("P")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
+            }
+        }
+        for (int i = n - 1; i > 0; i--) {
+            if (!garbage[i].contains("M")) {
+                ans -= travel[i - 1];
+            } else {
+                break;
+            }
+        }
+        return ans;
+    }
+}
--- src/ArraysQuestions/ReductionOperationToMakeArrayElementsEqual.java
+++ src/ArraysQuestions/ReductionOperationToMakeArrayElementsEqual.java
@@ -1,8 +1,29 @@
 package ArraysQuestions;
 
+import java.util.Arrays;
+
 public class ReductionOperationToMakeArrayElementsEqual
 {
     public static void main(String[] args) {
 
+        int[] nums = {5,5,3,3,6,7,8,9};
+        System.out.println("Min Operations : "+(reductionOperations(nums)));
+    }
+
+    static  public int reductionOperations(int[] nums)
+    {
+        int count=1;
+        int ans=0;
+
+        Arrays.sort(nums);
+
+        for(int i = nums.length-2; i >=0 ;i--)
+        {
+            if(nums[i] != nums[i+1]){
+                ans += count;
+            }
+            count++;
+        }
+        return ans;
     }
 }
